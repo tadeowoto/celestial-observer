a8721fe0f43c6f83129142b87487b8548c4d2fa9
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-19 19:22:36 -0300

    feat: main styles to PostList component

--- app/components/features/PostList.tsx
+++ app/components/features/PostList.tsx
@@ -1,9 +1,15 @@
 export default function PostList() {
   return (
-    <ul>
-      <li>Post 1</li>
-      <li>Post 2</li>
-      <li>Post 3</li>
-    </ul>
+    <section className="w-full h-fit min-h-screen  p-5">
+      <div className="w-full h-20 border-b border-outline-variant">
+        <p>Journal Feed</p>
+        <h2 className="text-3xl font-bold">Discovery the Journal</h2>
+      </div>
+      <ul className="w-full h-full grid grid-cols-1 place-items-center gap-10 p-5 mt-10">
+        <li>Post 1</li>
+        <li>Post 2</li>
+        <li>Post 3</li>
+      </ul>
+    </section>
   );
 }
