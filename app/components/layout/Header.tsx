1c2578b1f5239ab702e3340aab1404275a451aae
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-19 19:16:37 -0300

    feat: lucide react added and header styled

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,3 +1,18 @@
+import { Menu, CircleUser } from "lucide-react";
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="w-full  h-15">
+      <nav className="w-full h-full p-5 flex items-center justify-between">
+        <div>
+          <Menu />
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold">Celestial Observer</h1>
+        </div>
+        <div>
+          <CircleUser />
+        </div>
+      </nav>
+    </header>
+  );
 }
