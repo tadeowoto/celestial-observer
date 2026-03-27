99be1c52300ad21a3c77c3c6f570d34d9ec41616
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-27 10:35:15 -0300

    feat: getPostById endpoint to post.ts added

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,6 @@
 import { Post } from "@/types/types";
 
-export default async function getPosts(){
+export default async function getAllPosts(){
       const data = await fetch("http://localhost:8080/posts/user/1");
       const posts = await data.json();
 
@@ -19,4 +19,20 @@ export default async function getPosts(){
       })
       return mappedPosts
 
+}
+export async function getPostById(id: number) {
+  const data = await fetch(`http://localhost:8080/posts/${id}`);
+  const post = await data.json();
+  const mappedPost = {
+    title: post.title,
+    image_url: post.image_url,
+    celestial_body: post.celestial_body,
+    equipment: post.equipment,
+    created_at: post.created_at,
+    atmosphereCondition: post.atmosphereCondition,
+    description: post.description,
+    location: post.location,
+    postId: post.postId
+  }
+  return mappedPost;
 }
