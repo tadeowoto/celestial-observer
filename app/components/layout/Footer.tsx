66e9c34ab92ed4fd2c26062dbd291a0bffda9284
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-27 12:03:59 -0300

    feat: z index and links to other pages added

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Compass, Form, User } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="w-90 rounded-md text-on-surface  bottom-0 fixed h-16 flex justify-evenly items-center bg-background/60 backdrop-blur-lg border border-outline-variant">
+    <footer className="w-90 rounded-md z-100 text-on-surface  bottom-0 fixed h-16 flex justify-evenly items-center bg-background/60 backdrop-blur-lg border border-outline-variant">
       <Link
         href="/"
         className="flex flex-col items-center justify-center uppercase text-xs gap-1"
@@ -12,14 +12,14 @@ export default function Footer() {
         <p>Observe</p>
       </Link>
       <Link
-        href="/"
+        href="/entry"
         className="flex flex-col items-center justify-center uppercase text-xs gap-1"
       >
         <Form size={20} strokeWidth={1.5} />
         <p>Entry</p>
       </Link>
       <Link
-        href="/"
+        href="/profile"
         className="flex flex-col items-center justify-center uppercase text-xs gap-1"
       >
         <User size={20} strokeWidth={1.5} />
