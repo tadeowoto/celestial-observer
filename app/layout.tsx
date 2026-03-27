20a0eef0a09eafe7621cc275f1b23f4c6df233ba
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-27 12:02:02 -0300

    refactor: footer to rootLayout changed and removed from homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Inter } from "next/font/google";
 import "./globals.css";
-
+import Footer from "./components/layout/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +27,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col items-center">
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Footer from "./components/layout/Footer";
 import PostList from "./components/features/PostList";
 import Header from "./components/layout/Header";
 export default function Home() {
@@ -6,7 +5,6 @@ export default function Home() {
     <main className="w-full min-h-screen h-fit flex flex-col items-center">
       <Header />
       <PostList />
-      <Footer />
     </main>
   );
 }
