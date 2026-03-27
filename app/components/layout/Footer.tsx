6e12c344ca13a66d8073aae628be99724e85a328
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-27 12:00:05 -0300

    feat: nav styles and component added

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,3 +1,30 @@
+import Link from "next/link";
+import { Compass, Form, User } from "lucide-react";
+
 export default function Footer() {
-  return <footer className="hidden md:hidden lg:flex">Footer</footer>;
+  return (
+    <footer className="w-90 rounded-md text-on-surface  bottom-0 fixed h-16 flex justify-evenly items-center bg-background/60 backdrop-blur-lg border border-outline-variant">
+      <Link
+        href="/"
+        className="flex flex-col items-center justify-center uppercase text-xs gap-1"
+      >
+        <Compass size={25} strokeWidth={1.5} />
+        <p>Observe</p>
+      </Link>
+      <Link
+        href="/"
+        className="flex flex-col items-center justify-center uppercase text-xs gap-1"
+      >
+        <Form size={20} strokeWidth={1.5} />
+        <p>Entry</p>
+      </Link>
+      <Link
+        href="/"
+        className="flex flex-col items-center justify-center uppercase text-xs gap-1"
+      >
+        <User size={20} strokeWidth={1.5} />
+        <p>Profile</p>
+      </Link>
+    </footer>
+  );
 }
