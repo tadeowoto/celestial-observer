04111fe2768adefe9844a8e78f56a38a6e98d04d
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-27 10:45:50 -0300

    feat: server component to post page added, and validations for url path

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,8 +1,26 @@
-"use client";
+import { getPostById } from "@/lib/posts";
+import { notFound } from "next/navigation";
 
-import { useParams } from "next/navigation";
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
 
-export default function PostPage() {
-  const params = useParams();
-  return <div>Post {params.id}</div>;
+export default async function PostPage({ params }: PageProps) {
+  const { id } = await params;
+  const numericId = Number(id);
+
+  if (isNaN(numericId)) {
+    return notFound();
+  }
+
+  const post = await getPostById(numericId);
+
+  if (!post) {
+    return notFound();
+  }
+  return (
+    <main className="flex justify-center p-12 bg-background">
+      <h1>{post.title}</h1>
+    </main>
+  );
 }
