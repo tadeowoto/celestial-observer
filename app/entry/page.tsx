6e8bd0cd5e54df3f5ac5c87282ee52978fe099f1
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-31 09:16:24 -0300

    feat: form structure added

--- app/entry/page.tsx
+++ app/entry/page.tsx
@@ -1,7 +1,26 @@
 export default function entryPage() {
   return (
-    <section>
+    <section className="w-full min-h-screen h-fit flex flex-col items-center justify-start gap-4 p-4 bg-amber-300">
       <h1>Celestial Observer Form</h1>
+      <article className="w-full min-h-screen">
+        <form action="" className="flex flex-col gap-4 ">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" />
+          <label htmlFor="image-url">Image URL</label>
+          <input type="text" id="image-url" name="image-url" />
+          <div className="flex flex-col gap-4">
+            <label htmlFor="celestial-body">Celestial Body</label>
+            <input type="date" id="celestial-body" name="celestial-body" />
+            <label htmlFor="equipment">Equipment</label>
+            <input type="text" id="equipment" name="equipment" />
+            <label htmlFor="location">Created At</label>
+            <input type="datetime-local" id="location" name="location" />
+          </div>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" name="description" rows={5} />
+          <button>Submit</button>
+        </form>
+      </article>
     </section>
   );
 }
